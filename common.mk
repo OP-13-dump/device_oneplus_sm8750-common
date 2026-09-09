13f682e21d83edf31a1e80e215aa1d4ac2df95c6
dianlujitao <dianlujitao@lineageos.org>
2026-09-09 01:01:49 +0100

    sm8750-common: Add overlay for Nfc
    Change-Id: Ib5646b6573f45263422e863b48f1b11f5be5e633
    Signed-off-by: NoCache-69 <chethangowdasj86@gmail.com>

--- common.mk
+++ common.mk
@@ -560,7 +560,8 @@ PRODUCT_PACKAGES += \
 ifneq ($(TARGET_IS_TABLET),true)
 PRODUCT_PACKAGES += \
     CarrierConfigResCommon \
-    FrameworksResTargetPhone
+    FrameworksResTargetPhone \
+    NfcResCommon_Sys
 endif
 
 # Partitions
